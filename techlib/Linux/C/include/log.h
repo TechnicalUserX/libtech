04b3b18dc9d9a242a5a1ab041148f0b95af07456
TechnicalUserX <62398181+TechnicalUserX@users.noreply.github.com>
2021-10-26 14:36:18 +0300

    Add files via upload

---
+++ techlib/Linux/C/include/log.h
@@ -0,0 +1,34 @@
+ //===================================================================================================//
+/**
+ *  WRITER: TechnicaluserX
+ * 
+ *  VERSION: 1.0.0
+ *  
+ *  DESCRIPTION: Prints text onto the screen with time stamps.
+ *  
+ *  LICENSE: This library is not currently under any license and it is free to modify/distribute. 
+ * 
+ */
+ //===================================================================================================//
+
+
+#ifndef _TECHLIB_LOG_H
+#define _TECHLIB_LOG_H
+
+#include <stdio.h>
+#include <wchar.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <stdarg.h>
+
+
+
+// Logging functions
+
+void techlib_log_ascii(const char* display_string, ...);
+
+void techlib_log_widechar(const wchar_t* display_string, ...);
+
+#endif

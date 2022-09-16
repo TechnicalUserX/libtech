98538e17e4346da8bfabbf5002adf61a7ee8a764
TechnicalUserX <62398181+TechnicalUserX@users.noreply.github.com>
2022-09-16 23:18:02 +0300

    Delete techlib directory

--- techlib/Windows/C/src/color_cmd.c
+++
@@ -1,7 +0,0 @@
-#include "../include/color_cmd.h"
-
-TECHLIB_WINDLL_API void color(WORD color){
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, color);
-    return;
-}

9510fadbe697cbfaacf0520a3bfaaa2d267e4516
TechnicalUserX <62398181+TechnicalUserX@users.noreply.github.com>
2023-02-18 13:30:30 +0300

    Add files via upload

--- config/tech_features.h
+++ config/tech_features.h
@@ -1,34 +1,29 @@
 #ifndef TECH_FEATURES_H
 #define TECH_FEATURES_H
 
-
 // Supported standards: (> C11), (> C++11)
 // Supported compilers: gcc, clang
 
 #if !defined(__GNUC__) && !defined(__clang__)
-    #error "This library is only intended for gcc and clang compilers"
+#error "This library is only intended for gcc and clang compilers"
 #endif
 
 #ifdef __cplusplus
 
-    #if __cplusplus < 201103L
-        #error "Accepted minimum C++ standard is C++11"
-    #endif
+#if __cplusplus < 201103L
+#error "Accepted minimum C++ standard is C++11"
+#endif
 
 #else
 
-    #if __STDC_VERSION__ < 201112L
-        #error "Accepted minimum C standard is C11"
-    #endif
-
+#if __STDC_VERSION__ < 201112L
+#error "Accepted minimum C standard is C11"
 #endif
 
+#endif
 
 #if !defined(_GNU_SOURCE)
-    #define _GNU_SOURCE
+#define _GNU_SOURCE
 #endif
-
-
-
 
 #endif
